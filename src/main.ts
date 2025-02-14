0c93cb9fa9e5417c49e7a558781e4bf64999beef
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2025-02-14 15:05:52 -0500

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Hermes")
+    .setDescription("The Hermes API description")
+    .setVersion("0.1")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }));
