dafd98146ce7244f544c9580013a72cd59308131
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2025-02-17 18:54:09 -0500

    implement trycatch

--- src/modules/services/dto/create-service.dto.ts
+++ src/modules/services/dto/create-service.dto.ts
@@ -1,1 +1,15 @@
-export class CreateServiceDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateServiceDto {
+    @ApiProperty()
+    idCategoryServices: number;
+
+    @ApiProperty()
+    name: string;
+
+    @ApiProperty()
+    price: number;
+
+    @ApiProperty()
+    status: boolean;
+}
