d13fd14670c6c5a989db34793d474b91751583e2
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2025-02-17 18:39:30 -0500

    add services module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { CategoryServicesModule } from './modules/category-services/category-services.module';
 import { PrismaModule } from './config/prisma/prisma.module';
+import { ServicesModule } from './modules/services/services.module';
 
 @Module({
-  imports: [UsersModule, CategoryServicesModule, PrismaModule],
+  imports: [UsersModule, CategoryServicesModule, PrismaModule, ServicesModule],
   controllers: [AppController],
   providers: [AppService],
 })
