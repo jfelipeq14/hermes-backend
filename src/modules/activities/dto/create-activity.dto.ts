6522b37d5b565021b01185565c79ace5963e43bf
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2025-02-18 13:53:51 -0500

    add DTO Validations

--- src/modules/activities/dto/create-activity.dto.ts
+++ src/modules/activities/dto/create-activity.dto.ts
@@ -1,1 +1,9 @@
-export class CreateActivityDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateActivityDto {
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+}
