5df5507f51d1b8350ba878be241f168d67c5378d
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2025-02-18 14:12:02 -0500

    add validation in activities: DTO

--- src/modules/activities/dto/create-activity.dto.ts
+++ src/modules/activities/dto/create-activity.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateActivityDto {
-    @ApiProperty()
+    @ApiProperty({ required: true, description: 'Name of the activity' })
     @IsString()
     @IsNotEmpty()
     name: string;
