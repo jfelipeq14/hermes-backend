9b0fcaf7ad0783471ea0e297a40dc6a05e282f66
Andrey0108 <alanandrey234@gmail.com>
2025-02-20 09:46:36 -0500

    privileges review

--- src/modules/privileges/privileges.controller.ts
+++ src/modules/privileges/privileges.controller.ts
@@ -1,32 +1,65 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { PrivilegesService } from './privileges.service';
+import { CreatePrivilegeDto } from './dto/create-privilege.dto';
+import { UpdatePrivilegeDto } from './dto/update-privilege.dto';
 
 @Controller('privileges')
 export class PrivilegesController {
   constructor(private readonly privilegesService: PrivilegesService) {}
 
-  // @Post()
-  // create(@Body() createPrivilegeDto: CreatePrivilegeDto) {
-  //   return this.privilegesService.create(createPrivilegeDto);
-  // }
-
   @Get()
   findAll() {
-    return this.privilegesService.findAll();
+    try {
+      return this.privilegesService.findAll();
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.privilegesService.findOne(+id);
-  // }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    try {
+      return this.privilegesService.findOne(+id);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updatePrivilegeDto: UpdatePrivilegeDto) {
-  //   return this.privilegesService.update(+id, updatePrivilegeDto);
-  // }
+  @Post()
+  create(@Body() createPrivilegeDto: CreatePrivilegeDto) {
+    try {
+      return this.privilegesService.create(createPrivilegeDto);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.privilegesService.remove(+id);
-  // }
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updatePrivilegeDto: UpdatePrivilegeDto,
+  ) {
+    try {
+      return this.privilegesService.update(+id, updatePrivilegeDto);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    try {
+      return this.privilegesService.remove(+id);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
