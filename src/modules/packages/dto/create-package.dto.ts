3c142605fb7aab4e67fe27ad16dfd78c9b1169c3
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2025-02-27 15:22:31 -0500

    format

--- src/modules/packages/dto/create-package.dto.ts
+++ src/modules/packages/dto/create-package.dto.ts
@@ -1,6 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
 import {
-  IsBoolean,
   IsDateString,
   IsInt,
   IsNotEmpty,
--- src/modules/services/dto/create-service.dto.ts
+++ src/modules/services/dto/create-service.dto.ts
@@ -1,6 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
 import {
-  IsBoolean,
   IsInt,
   IsNotEmpty,
   IsNumber,
