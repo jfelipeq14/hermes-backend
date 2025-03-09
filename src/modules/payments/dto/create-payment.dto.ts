ca3e1915b81a3de79e7a166f491df5ac230b8ccd
Bryan0813 <brayansanchez0813@gmail.com>
2025-03-09 00:03:29 -0500

    add payments module with regex

--- src/modules/payments/dto/create-payment.dto.ts
+++ src/modules/payments/dto/create-payment.dto.ts
@@ -1,48 +1,45 @@
 import { ApiProperty } from '@nestjs/swagger';
 import {
-    IsDateString,
-    IsInt,
-    IsNotEmpty,
-    IsNumber,
-    IsString,
-    IsBoolean,
-    Matches
+  IsDateString,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsBoolean,
+  Matches,
 } from 'class-validator';
 import {
-    IsNumberRegex,
-    IsDateRegex,
-    IsStringRegex,
-    IsPriceRegex,
+  IsDateRegex,
+  IsPriceRegex,
+  ImageExtensionRegex,
 } from 'src/utils/regex';
 
-
 export class CreatePaymentDto {
-    @ApiProperty({ required: true, description: 'Date of the payment' })
-    @IsNotEmpty()
-    @IsInt()
-    @Matches(IsNumberRegex)
-    idReservation: number;
+  @ApiProperty({ required: true, description: 'Date of the payment' })
+  @IsNotEmpty()
+  @IsInt()
+  idReservation: number;
 
-    @ApiProperty({ required: true, description: 'Date of the payment' })
-    @IsDateString()
-    @IsNotEmpty()
-    @Matches(IsDateRegex)
-    date: Date;
+  @ApiProperty({ required: true, description: 'Date of the payment' })
+  @IsDateString()
+  @IsNotEmpty()
+  @Matches(IsDateRegex)
+  date: Date;
 
-    @ApiProperty({ required: true, description: 'Price of the payment' })
-    @IsNumber()
-    @IsNotEmpty()
-    @Matches(IsPriceRegex)
-    price: number;
+  @ApiProperty({ required: true, description: 'Price of the payment' })
+  @IsNumber()
+  @IsNotEmpty()
+  @Matches(IsPriceRegex)
+  price: number;
 
-    @ApiProperty({ required: true, description: 'Voucher of the payment' })
-    @IsString()
-    @IsNotEmpty()
-    @Matches(IsStringRegex)
-    voucher: string;
+  @ApiProperty({ required: true, description: 'Voucher of the payment' })
+  @IsString()
+  @IsNotEmpty()
+  @Matches(ImageExtensionRegex)
+  voucher: string;
 
-    @ApiProperty({ required: true, description: 'Status of the payment' })
-    @IsBoolean()
-    @IsNotEmpty()
-    status: boolean;
+  @ApiProperty({ required: true, description: 'Status of the payment' })
+  @IsBoolean()
+  @IsNotEmpty()
+  status: boolean;
 }
--- src/modules/payments/payments.service.ts
+++ src/modules/payments/payments.service.ts
@@ -6,33 +6,33 @@ import { PrismaService } from 'src/config/prisma/prisma.service';
 @Injectable()
 export class PaymentsService {
   constructor(private prisma: PrismaService) {}
-  
-    findAll() {
-      return this.prisma.payments.findMany();
-    }
-  
-    findOne(id: number) {
-      return this.prisma.payments.findUnique({
-        where: { id },
-      });
-    }
-  
-    create(createPaymentDto: CreatePaymentDto) {
-      return this.prisma.payments.create({
-        data: createPaymentDto,
-      });
-    }
-  
-    update(id: number, updatePaymentDto: UpdatePaymentDto) {
-      return this.prisma.payments.update({
-        where: { id },
-        data: updatePaymentDto,
-      });
-    }
-  
-    remove(id: number) {
-      return this.prisma.payments.delete({
-        where: { id },
-      });
-    }
+
+  findAll() {
+    return this.prisma.payments.findMany();
   }
+
+  findOne(id: number) {
+    return this.prisma.payments.findUnique({
+      where: { id },
+    });
+  }
+
+  create(createPaymentDto: CreatePaymentDto) {
+    return this.prisma.payments.create({
+      data: createPaymentDto,
+    });
+  }
+
+  update(id: number, updatePaymentDto: UpdatePaymentDto) {
+    return this.prisma.payments.update({
+      where: { id },
+      data: updatePaymentDto,
+    });
+  }
+
+  remove(id: number) {
+    return this.prisma.payments.delete({
+      where: { id },
+    });
+  }
+}
